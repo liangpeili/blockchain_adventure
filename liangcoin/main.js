f53be61191233789be9c524c9044c6fafba8ee4b
liangpeili <liangpeili@asch.so>
2017-09-28 20:29:29 +0800

    update liangcoin

--- liangcoin/main.js
+++ liangcoin/main.js
@@ -1,6 +1,6 @@
 const SHA256 = require('crypto-js/sha256');
 
-class Block{
+class Block {
   constructor(index, timestamp, data, previousHash = '') {
     this.index = index;
     this.timestamp = timestamp;
@@ -14,26 +14,26 @@ class Block{
   }
 }
 
-class Blockchain{
-  constructor(){
+class Blockchain {
+  constructor() {
     this.chain = [this.createGenesisBlock()];
   }
 
-  createGenesisBlock(){
+  createGenesisBlock() {
     return new Block(0, "01/01/2017", "Genesis block", "0");
   }
 
-  getLatestBlock(){
+  getLatestBlock() {
     return this.chain[this.chain.length - 1];
   }
 
-  addBlock(newBlock){
+  addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
     newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
 
-  isChainValid(){
+  isChainValid() {
     for (let i = 1; i < this.chain.length; i++){
       const currentBlock = this.chain[i];
       const previousBlock = this.chain[i - 1];
@@ -50,7 +50,7 @@ class Blockchain{
   }
 }
 
-let liangCoin = new Blockchain();
+const liangCoin = new Blockchain();
 liangCoin.addBlock(new Block(1, "10/07/2017", { amount: 4 }));
 liangCoin.addBlock(new Block(2, "12/07/2017", { amount: 10 }));
 
