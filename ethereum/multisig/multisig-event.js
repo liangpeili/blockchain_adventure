0fbd20466a5c2287e202d292a25033f72d7e57eb
liangpeili <liangpeili@foxmail.com>
2017-10-19 21:18:26 +0800

    upate multisig event

--- ethereum/multisig/multisig-event.js
+++ ethereum/multisig/multisig-event.js
@@ -5,13 +5,13 @@ web3.setProvider(new web3.providers.HttpProvider(ethereumUri));
 const abi = require('./multisig-abi.json');
 
 const MyContract = web3.eth.contract(abi);
-const myContractInstance = MyContract.at('0xb7feffd5a96c122b1633b99a6f1f18ae3d691112');
+const myContractInstance = MyContract.at('0xD138C1E3F6CcAFaa9E2235837EB049D371C0A745');
 
-const event = myContractInstance.Deposit();
+const events = myContractInstance.allEvents();
 
 // watch for changes
-event.watch(function(error, result){
+events.watch(function(error, result){
   if (!error) {
-    console.log(result);
+    console.log(JSON.stringify(result, undefined, 2));
   }    
 });
